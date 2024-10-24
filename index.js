23bef344f6f4af33fd561471e609ecb10a8973c1
Steven <stevenaknobel@gmail.com>
2024-10-24 12:58:41 +0200

    updated starting parameters const to be cleaner and have more descriptive names

--- index.js
+++ index.js
@@ -6,13 +6,14 @@
  */
 
 // Given Parameters
-const vel = 10000; // velocity (km/h)
-const acc = 3; // acceleration (m/s^2)
-const time = 3600; // seconds (1 hour)
-const d = 0; // distance (km)
-const fuel = 5000; // remaining fuel (kg)
-const fbr = 0.5; // fuel burn rate (kg/s)
-
+const parameters = {
+  velocity: 10000, // velocity (km/h)
+  acceleration: 3, // acceleration (m/s^2)
+  time: 3600, // seconds (1 hour)
+  distance: 0, //distance (km)
+  fuel: 5000, // remaining fuel (kg)
+  fuelBurnRate: 0.5 // fuel burn rate (kg/s)
+}
 
 const d2 = d + (vel*time) //calcultes new distance
 const rf = fbr*time //calculates remaining fuel
